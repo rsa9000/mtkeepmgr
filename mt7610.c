17d08be73e6f7fa0718e9d07da6615c175e78dac
Sergey Ryazanov <ryazanov.s.a@gmail.com>
2016-10-01 01:24:49 +0300

    mt7610: print device identification info (MAC/PCI ID(s))

--- mt7610.c
+++ mt7610.c
@@ -4,11 +4,22 @@
  * Copyright (c) 2016, Sergey Ryazanov <ryazanov.s.a@gmail.com>
  */
 
+#include <stdio.h>
+
 #include "medump.h"
+#include "utils.h"
 #include "mt7610.h"
 
 static int mt7610_eep_parse(void)
 {
+	printf("[Device identification]\n");
+	printf("  MacAddr       : %s\n", get_macaddr_str());
+	printf("  PCIDevID      : 0x%04x\n", eep_read_word(E_PCI_DEV_ID));
+	printf("  PCIVenID      : 0x%04x\n", eep_read_word(E_PCI_VEN_ID));
+	printf("  PCISubsysDevID: 0x%04x\n", eep_read_word(E_PCI_SUB_DEV_ID));
+	printf("  PCISubsysVenID: 0x%04x\n", eep_read_word(E_PCI_SUB_VEN_ID));
+	printf("\n");
+
 	return 0;
 }
 
--- mt7610.h
+++ mt7610.h
@@ -7,4 +7,10 @@
 #ifndef _MT7610_H_
 #define _MT7610_H_
 
+#define E_PCI_DEV_ID		0x000a
+#define E_PCI_VEN_ID		0x000c
+
+#define E_PCI_SUB_DEV_ID	0x0012
+#define E_PCI_SUB_VEN_ID	0x0014
+
 #endif	/* !_MT7610_H_ */
