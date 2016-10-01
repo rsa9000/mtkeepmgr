3507c56dd5e571120a01134ea578c8e3408764c4
Sergey Ryazanov <ryazanov.s.a@gmail.com>
2016-10-02 01:54:32 +0300

    mt7610: add NIC config 0/1/2 dumping

--- mt7610.c
+++ mt7610.c
@@ -12,12 +12,44 @@
 
 static int mt7610_eep_parse(void)
 {
+	uint16_t val;
+
 	printf("[Device identification]\n");
 	printf("  MacAddr       : %s\n", get_macaddr_str());
 	printf("  PCIDevID      : 0x%04x\n", eep_read_word(E_PCI_DEV_ID));
 	printf("  PCIVenID      : 0x%04x\n", eep_read_word(E_PCI_VEN_ID));
 	printf("  PCISubsysDevID: 0x%04x\n", eep_read_word(E_PCI_SUB_DEV_ID));
 	printf("  PCISubsysVenID: 0x%04x\n", eep_read_word(E_PCI_SUB_VEN_ID));
+	printf("\n");
+
+	printf("[Device configuration]\n");
+	val = eep_read_word(E_NIC_CFG0);
+	printf("  RxPath        : %u\n", FIELD_GET(E_NIC_CFG0_RX_PATH, val));
+	printf("  TxPath        : %u\n", FIELD_GET(E_NIC_CFG0_TX_PATH, val));
+	printf("  PA 2GHz       : %s\n", val & E_NIC_CFG0_INT_2G_PA ? "Internal" : "External");
+	printf("  PA 5GHz       : %s\n", val & E_NIC_CFG0_INT_5G_PA ? "Internal" : "External");
+	printf("  PA current    : %u ma\n", val & E_NIC_CFG0_EXT_PA_CURR ? 8 : 16);
+	val = eep_read_word(E_NIC_CFG1);
+	printf("  RF Ctrl       : %s\n", val & E_NIC_CFG1_HW_RF_CTRL ? "Hw" : "Driver");
+	printf("  DynTxAgcCtrl  : %s\n", val & E_NIC_CFG1_DYN_TX_AGC ? "Enable" : "Disable");
+	printf("  LNA 2GHz      : %s\n", val & E_NIC_CFG1_EXT_2G_LNA ? "External" : "Internal");
+	printf("  LNA 5GHz      : %s\n", val & E_NIC_CFG1_EXT_5G_LNA ? "External" : "Internal");
+	printf("  CardBus Accel : %s\n", val & E_NIC_CFG1_CB_ACCEL_DIS ? "Disable" : "Enable");
+	printf("  40MHZ 2GHz SB : %s\n", val & E_NIC_CFG1_40M_2G_SB ? "Enable" : "Disable");
+	printf("  40MHZ 5GHz SB : %s\n", val & E_NIC_CFG1_40M_5G_SB ? "Enable" : "Disable");
+	printf("  WPS button    : %s\n", val & E_NIC_CFG1_WPS_BUT_EN ? "Enable" : "Disable");
+	printf("  40MHz 2GHz    : %s\n", val & E_NIC_CFG1_40M_2G_DIS ? "Disable" : "Enable");
+	printf("  40MHz 5GHz    : %s\n", val & E_NIC_CFG1_40M_5G_DIS ? "Disable" : "Enable");
+	printf("  Ant. diversity: %s\n", val & E_NIC_CFG1_ANT_DIV ? "True" : "False");
+	printf("  Ant. opt.     : %s\n", val & E_NIC_CFG1_ANT_OPT ? "True" : "False");
+	printf("  Internal TxALC: %s\n", val & E_NIC_CFG1_INT_TX_ALC ? "True" : "False");
+	printf("  Coexistance   : %s\n", val & E_NIC_CFG1_COEX ? "True" : "False");
+	val = eep_read_word(E_NIC_CFG2);
+	printf("  RxStream      : %u\n", FIELD_GET(E_NIC_CFG2_RX_STREAM, val));
+	printf("  TxStream      : %u\n", FIELD_GET(E_NIC_CFG2_TX_STREAM, val));
+	printf("  CoexAnt       : %s\n", val & E_NIC_CFG2_COEX_ANT ? "True" : "False");
+	printf("  XtalOpt       : %u\n", FIELD_GET(E_NIC_CFG2_XTAL_OPT, val));
+	printf("  RxTempCompens.: %s\n", val & E_NIC_CFG2_RXTEMP_C_DIS ? "Disable" : "Enable");
 	printf("\n");
 
 	return 0;
--- mt7610.h
+++ mt7610.h
@@ -13,4 +13,40 @@
 #define E_PCI_SUB_DEV_ID	0x0012
 #define E_PCI_SUB_VEN_ID	0x0014
 
+#define E_NIC_CFG0		0x0034
+#define E_NIC_CFG0_RX_PATH	0x000f
+#define E_NIC_CFG0_RX_PATH_S	0
+#define E_NIC_CFG0_TX_PATH	0x00f0
+#define E_NIC_CFG0_TX_PATH_S	4
+#define E_NIC_CFG0_INT_2G_PA	0x0100
+#define E_NIC_CFG0_INT_5G_PA	0x0200
+#define E_NIC_CFG0_EXT_PA_CURR	0x0400
+
+#define E_NIC_CFG1		0x0036
+#define E_NIC_CFG1_HW_RF_CTRL	0x0001	/* Hardware radio control */
+#define E_NIC_CFG1_DYN_TX_AGC	0x0002	/* Dynamic Tx AGC */
+#define E_NIC_CFG1_EXT_2G_LNA	0x0004
+#define E_NIC_CFG1_EXT_5G_LNA	0x0008
+#define E_NIC_CFG1_CB_ACCEL_DIS	0x0010	/* Dis. cardbus acceleration */
+#define E_NIC_CFG1_40M_2G_SB	0x0020	/* Sideband 40MHz BW in 2.4 GHz */
+#define E_NIC_CFG1_40M_5G_SB	0x0040	/* Sideband 40MHz BW in 5 GHz */
+#define E_NIC_CFG1_WPS_BUT_EN	0x0080	/* Enable WPS button */
+#define E_NIC_CFG1_40M_2G_DIS	0x0100	/* Dis. 40MHz in 2.4 GHz */
+#define E_NIC_CFG1_40M_5G_DIS	0x0200	/* Dis. 40MHz in 5 GHz */
+#define E_NIC_CFG1_ANT_DIV	0x0800	/* Antenna diversity */
+#define E_NIC_CFG1_ANT_OPT	0x1000
+#define E_NIC_CFG1_INT_TX_ALC	0x2000	/* Internal Tx ALC */
+#define E_NIC_CFG1_COEX		0x4000
+#define E_NIC_CFG1_DAC_TEST	0x8000
+
+#define E_NIC_CFG2		0x0042
+#define E_NIC_CFG2_RX_STREAM	0x000f
+#define E_NIC_CFG2_RX_STREAM_S	0
+#define E_NIC_CFG2_TX_STREAM	0x00f0
+#define E_NIC_CFG2_TX_STREAM_S	4
+#define E_NIC_CFG2_COEX_ANT	0x0100
+#define E_NIC_CFG2_XTAL_OPT	0x0600
+#define E_NIC_CFG2_XTAL_OPT_S	9
+#define E_NIC_CFG2_RXTEMP_C_DIS	0x0800	/* Rx temp compensation dis. */
+
 #endif	/* !_MT7610_H_ */
